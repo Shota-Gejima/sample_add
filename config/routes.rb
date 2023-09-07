34c0d1956802f1214f8ca9aac4ada25f7de0081c
EC2 Default User <143868019+Shota-Gejima@users.noreply.github.com>
2023-09-07 08:05:11 +0000

    [finish] 3-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
   
-# HTTPﾒｿｯﾄﾞ　'コントローラー/アクション'=> 'テーブル名/アクション名'
-  get 'lists/new'
+# HTTPﾒｿｯﾄﾞ　'URI'=> 'コントローラー名/アクション名'
   get '/top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show' ,as:'list'
-  get 'lists/:id/edit' => 'lists#edit' ,as:'edit_list'
-  patch 'lists/:id' => 'lists#update' ,as: 'update_list'
-  delete 'lists/:id' => 'lists#destroy' ,as: 'destroy_list'
+  resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
